f0da8f8bb8ecd395f978cb645276dc93e4fe4356
Charles Anthony <charles.unix.pro@gmail.com>
2018-06-01 11:08:54 -0700

    Add iox_$modes support and modes.c example code.

---
+++ modes.c
@@ -0,0 +1,21 @@
+main(argc, argv) 
+  int argc;
+  char * argv [];
+{
+  /* char * new_modes = "ll132"; */
+  char old_modes [513];
+  int i, code;
+  struct iocb * iocbp = 0;
+
+  c_iox_find_iocb ("user_output", & iocbp, & code);
+  printf ("find_iocb code %d ptr %lo\n", code, iocbp);
+  c_iox_modes (iocbp, argv[1], old_modes, & code);
+  printf ("modes code %d\n", code);
+  old_modes [512] = 0;
+  for (i = 511; i >= 0; i --)
+    if (old_modes[i] != ' ')
+      break;
+  old_modes[i+1] = 0;
+  printf ("old <%s>\n", old_modes);
+  return 0;
+}
